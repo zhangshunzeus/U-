e4f6d9c67b14c7fc5a45ca37dd34ad0c132b30e5
aopeners <JiangKaiquan@Outlook.com>
2016-04-06 14:39:03 +0800

    fdsfd

---
+++ app/src/main/java/com/uauction/activity/HomePageActivity.java
@@ -0,0 +1,16 @@
+package com.uauction.activity;
+
+import com.example.ushooting.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class HomePageActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_home_page);
+	}
+
+}
